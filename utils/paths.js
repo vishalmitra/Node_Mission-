1148a1ad913c02713cca523f9f065c46c1959885
chebrolu vishal mitra <vishalmithra7@gmail.com>
2023-11-18 15:27:02 +0530

    Delete utils directory

--- utils/paths.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-
-console.log( path.dirname(require.main.filename))
-//module.exports = path.dirname(require.main.filename);
-
-// this is make the code look cleaner for the __dirman in path.join () function for renderint the html files
