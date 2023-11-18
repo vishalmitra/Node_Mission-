8cc1ba54c45d6ed13a77855d874ac759ff68bff6
chebrolu vishal mitra <vishalmithra7@gmail.com>
2023-11-18 15:27:44 +0530

    Delete controllers directory

--- controllers/error.js
+++
@@ -1,5 +0,0 @@
-exports.page404 =(req, res, next) => {
-    //res.status(404).send('<h1> page not found </h1>');
-    //res.status(404).sendFile(path.join(__dirname,"./","views","404.html"))});
-    res.status(404).render('404', {doctitle:"not found",path : "/admin/form"})
-} 
